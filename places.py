a2338d4af632ed7f0448a5be070e093228a6b927
lukesed <lukesedney+bitbucket@gmail.com>
2013-04-13 06:44:46 -0700

    d

--- places.py
+++ places.py
@@ -8,6 +8,19 @@ loc_url = 'https://maps.googleapis.com/maps/api/geocode/json'
 loc_resp = requests.get(loc_url, params=loc_query)
 coord = str(loc_resp.json()['results'][0]['geometry']['location']['lat']) + ',' + str(loc_resp.json()['results'][0]['geometry']['location']['lng'])
 
+# get price category from $ value
+dollars = 75; # todo
+if dollars > 55:
+  price = 4
+elif dollars > 35:
+  price = 3
+elif dollars > 20:
+  price = 2
+elif dollars > 10:
+  price = 1
+else:
+  price = 0
+
 # get nearby places from google
 config = ConfigParser.RawConfigParser()
 config.read('keys.cfg')
@@ -16,15 +29,14 @@ places_query['key'] = config.get('Keys', 'places')
 places_query['location'] = coord
 places_query['sensor'] = 'false'
 places_query['radius'] = 50000  #todo, maximum meters distance
-places_query['minprice'] = '1'  #todo, range 0 to 4
-places_query['maxprice'] = '2'  #todo
+places_query['minprice'] = 0
+places_query['maxprice'] = price
 places_query['types'] = 'amusement_park|aquarium|art_gallery|bakery|bar|bowling_alley|cafe|casino|food|movie_theater|museum|night_club|park|restaurant|shopping_mall|stadium|zoo'
 places_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 places_resp = requests.get(places_url, params=places_query)
-len(places_resp.json()['results'])
 
 # get place details
-details = {}
+details = []
 for x in places_resp.json()['results']:
   detail_query = {}
   detail_query['key'] = config.get('Keys', 'places')
@@ -32,38 +44,50 @@ for x in places_resp.json()['results']:
   detail_query['sensor'] = 'false'
   detail_url = 'https://maps.googleapis.com/maps/api/place/details/json'
   detail_resp = requests.get(detail_url, params=detail_query)
-  details[x['reference']] = detail_resp.json()['result']
+  details.append(detail_resp.json()['result'])
 
 # filter by open and closing time
 day = 0  #todo, 0 is sunday
 start = '1500'  #todo, military time
 end = '1700'  #todo
-filtered = {}
-for ref in details:
-  if 'opening_hours' in details[ref]:
-    hours = details[ref]['opening_hours']['periods'][day]
+filtered = []
+for x in details:
+  if 'opening_hours' in x:
+    hours = x['opening_hours']['periods'][day]
     if 'open' in hours and 'close' in hours:
       if 'time' in hours['open'] and 'time' in hours['close']:
         if (end > hours['open']['time']) and (start < hours['close']['time']):
-          filtered[ref] = details[ref]
+          filtered.append(x)
 
 # sort by rating
-rated = {}
-for ref in filtered:
-  if 'rating' in filtered[ref]:
-    rated[ref] = filtered[ref]
+rated = []
+for x in filtered:
+  if 'rating' in x:
+    rated.append(x)
+
 # if num entries are rated, use those
 best = []
 num = 3
 if len(rated) >= num:
-  for x in sorted(rated.items(), key=lambda e: e[1]['rating'])[-num:]:
-    print x[1]['rating']
+  for x in sorted(rated, key=lambda e: e['rating'])[-num:]:
+    print x['rating']
     best.append(x)
 # if not, use the first num returned
 else:
-  for x in filtered.items()[:num]:
-    print x[1]['rating']
+  for x in filtered[:num]:
     best.append(x)
 
+# location
 for x in best:
-  print str(x[1]['geometry']['location']['lat']) + ',' + str(x[1]['geometry']['location']['lng'])
+  print str(x['geometry']['location']['lat']) + ',' + str(x['geometry']['location']['lng'])
+
+# page url 
+for x in best:
+  print x['url']
+
+# rating
+for x in best:
+  if 'rating' in x:
+    print x['rating']
+
+
